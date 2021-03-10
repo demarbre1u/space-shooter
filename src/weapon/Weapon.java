5d76970471ea428f151fc1dfc13e8006ca89d9bf
Allan Demarbre <ademarbre@groupe-cac.com>
2021-03-10 16:21:18 +0100

    Adding javadoc on the Weapon and WeaponTripleShot classes

--- src/weapon/Weapon.java
+++ src/weapon/Weapon.java
@@ -4,30 +4,58 @@ import data.EntityType;
 import entity.Missile;
 import main.Main;
 
+/**
+ * Weapon class
+ */
 public class Weapon 
 {
+	/**
+	 * The minimum delay to wait before the next shot
+	 */
 	protected int shotDelay;
-	
+
+	/**
+	 * Weapon constructor
+	 */
 	public Weapon()
 	{
 		shotDelay = 15;
 	}
 
+	/**
+	 * Spawns a missile
+	 *
+	 * @param x
+	 * 		The x position of the missile
+	 * @param y
+	 * 		The y position of the missile
+	 * @param w
+	 * 		The width of the missile
+	 * @param h
+	 * 		The height of the missile
+	 */
 	public void spawnMissile(int x, int y, int w, int h) 
 	{
 		Main.entity.add(new Missile(x+w, y+(h/2 - 2), 15, 4, 8, 0, EntityType.Missile));
 	}
 
+	/**
+	 * Return the shot delay
+	 *
+	 * @return int
+	 * 		The shot delay
+	 */
 	public int getShotDelay() 
 	{
 		return shotDelay;
 	}
 
-	public void setShotDelay(int shotDelay) 
-	{
-		this.shotDelay = shotDelay;
-	}
-	
+	/**
+	 * Returns the string representation of the weapon
+	 *
+	 * @return String
+	 * 		The string representation of the weapon
+	 */
 	public String toString()
 	{
 		return "normal";
--- src/weapon/WeaponTripleShot.java
+++ src/weapon/WeaponTripleShot.java
@@ -4,13 +4,31 @@ import data.EntityType;
 import entity.Missile;
 import main.Main;
 
+/**
+ * WeaponTripleShot class
+ */
 public class WeaponTripleShot extends Weapon
 {
+	/**
+	 * WeaponTripleShot constructor
+	 */
 	public WeaponTripleShot() 
 	{
 		shotDelay = 25;
 	}
 
+	/**
+	 * Spawns a missile
+	 *
+	 * @param x
+	 * 		The x position of the missile
+	 * @param y
+	 * 		The y position of the missile
+	 * @param w
+	 * 		The width of the missile
+	 * @param h
+	 * 		The height of the missile
+	 */
 	public void spawnMissile(int x, int y, int w, int h) 
 	{
 		int scatterY = 1;
@@ -18,8 +36,14 @@ public class WeaponTripleShot extends Weapon
 		Main.entity.add(new Missile(x+w, y+(h/2 - 2), 15, 4, 8, scatterY, EntityType.Missile));
 		Main.entity.add(new Missile(x+w, y+(h/2 - 2), 15, 4, 8, 0, EntityType.Missile));
 		Main.entity.add(new Missile(x+w, y+(h/2 - 2), 15, 4, 8, -scatterY, EntityType.Missile));
-	} 
-	
+	}
+
+	/**
+	 * Returns the string representation of the weapon
+	 *
+	 * @return String
+	 * 		The string representation of the weapon
+	 */
 	public String toString()
 	{
 		return "triple shot";
